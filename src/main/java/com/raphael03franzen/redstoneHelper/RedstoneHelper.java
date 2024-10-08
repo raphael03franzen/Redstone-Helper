0f7cd0934a4bcecc08ea9b3034236a7758f9712d
Raphael Franzen <franzen.raphael@hotmail.com>
2024-10-08 16:37:38 +0200

    First Commit Boilerplate +Plugin Loaded Message

---
+++ src/main/java/com/raphael03franzen/redstoneHelper/RedstoneHelper.java
@@ -0,0 +1,17 @@
+package com.raphael03franzen.redstoneHelper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RedstoneHelper extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		System.out.println("Redstone Helper Loaded");
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
