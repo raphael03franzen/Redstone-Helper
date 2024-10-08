0894959ff1b9faf88b09a988b942ee3ef55c785e
Raphael Franzen <franzen.raphael@hotmail.com>
2024-10-08 21:04:03 +0200

    Implements BarrelCommand(/barrel int)

--- src/main/java/com/raphael03franzen/redstoneHelper/RedstoneHelper.java
+++ src/main/java/com/raphael03franzen/redstoneHelper/RedstoneHelper.java
@@ -2,12 +2,14 @@ package com.raphael03franzen.redstoneHelper;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class RedstoneHelper extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
 		System.out.println("Redstone Helper Loaded");
-
+		Objects.requireNonNull(this.getCommand("barrel")).setExecutor(new BarrelCommand());
 	}
 
 	@Override
